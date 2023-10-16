b523d13316ef977d9261d177030f8a830e9eccd3
Nikita Alpatov <nikitaa95@mail.ru>
2023-10-16 20:33:10 +0300

    feat: add card completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,6 @@ import { Card } from "@/components/ui/card";
 
 export function App() {
   return <div>
-    <Card className={"someNmae"} title={"cardtitle"}>Text</Card>
+    <Card  className={"someNmae"} title={"cardtitle"}>Text</Card>
   </div>
 }
--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,25 +1,19 @@
-import { ComponentProps, ReactNode } from 'react'
-import s from "@/components/ui/button/button.module.scss";
+import { ComponentProps, ReactNode } from 'react';
+import s from "@/components/ui/card/card.module.scss";
 
-
-type CommonProps = {
-  children: ReactNode
-} & ComponentProps<'div'>
-
-type ConditionalProps = {
-  icon?: ReactNode
-  title?: string
-}
-
-export type CardProps = CommonProps & ConditionalProps
+type CardProps = {
+  icon?: ReactNode;
+  title?: string;
+  className?: string;
+} & ComponentProps<'div'>;
 
 export const Card = (props: CardProps) => {
-  const { children, className, icon, title, ...rest }= props
+  const { children, className, icon, title, ...rest } = props;
 
   return (
-    <div className={` ${className}`} title={title}{...rest}>
-      {icon && <span className={s.icon}>{icon}</span>}
+    <div className={`${s.card} ${className ?? ''}`} {...rest}>
+      {icon && <div className={s.icon}>{icon}</div>}
       {children}
     </div>
-  )
-}
+  );
+};
