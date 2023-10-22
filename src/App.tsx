7bc11c06ef65fe09f773ee214ba0b806e32def59
Nikita Alpatov <nikitaa95@mail.ru>
2023-10-22 18:41:37 +0300

    feat: add disabled to checkbox(unfinished)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-
-import { MyButton } from "@/components/ui/Button";
-import MyCheckbox from "@/components/ui/Сheckbox/MyCheckbox.tsx";
+import Checkbox from "@/components/ui/Сheckbox/Checkbox.tsx";
 
 
 export function App() {
   return <div>
-    <MyCheckbox>checkxobx</MyCheckbox>
-    <MyButton variant={"link"} className={"kakoito"}>text</MyButton>
-    <MyButton variant={"primary"}>text</MyButton>
-    <MyButton variant={"secondary"}>text</MyButton>
-    <MyButton variant={"tertiary"}>text</MyButton>
+    <div>smth</div>
+    smth
+    <Checkbox  label={"label"} className={"className"}>checkxobx</Checkbox>
   </div>
 }
