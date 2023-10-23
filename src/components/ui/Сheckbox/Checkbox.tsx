0da98a8c7531ac3368af26c49723484233cd8fd4
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-10-23 23:56:01 +0500

    rewrite input.tsx, fix conflicts after pull from dev. fix checkbox indicator props. Add storybook static to gitignore

--- src/components/ui/Сheckbox/Checkbox.tsx
+++ src/components/ui/Сheckbox/Checkbox.tsx
@@ -1,38 +1,38 @@
-import * as CheckboxRadix from '@radix-ui/react-checkbox';
-import { CheckIcon } from '@radix-ui/react-icons';
-import s from './Checkbox.module.scss';
-import { ComponentPropsWithoutRef, ElementType } from "react";
-import { clsx } from "clsx";
+import { ComponentPropsWithoutRef, ElementType } from 'react'
 
+import * as CheckboxRadix from '@radix-ui/react-checkbox'
+import { CheckIcon } from '@radix-ui/react-icons'
+import { clsx } from 'clsx'
+
+import s from './Checkbox.module.scss'
 
 export type MyCheckboxProps<T extends ElementType> = {
-  title?: string
-  label?: string
-  onChange?: () => void
   checked?: boolean
   className?: string
   disabled?: boolean
+  label?: string
+  onChange?: () => void
+  title?: string
   //todo when disabled and checked background doesn't change.
 } & ComponentPropsWithoutRef<T>
 
-
 const Checkbox = <T extends ElementType = 'button'>(props: MyCheckboxProps<T>) => {
-  const { label, className, disabled, ...rest} = props
-  const checkBoxClassName = clsx(s.button,s.className, disabled && s.disabled)
+  const { className, disabled, label, ...rest } = props
+  const checkBoxClassName = clsx(s.button, s.className, disabled && s.disabled)
   const checkboxLabelClassName = clsx(s.Label, disabled && s.disabled)
 
   return (
     <div className={s.checkboxContainer}>
-      <CheckboxRadix.Root className={checkBoxClassName} disabled={disabled} id="c1" {...rest}>
-        <CheckboxRadix.Indicator disabled={disabled} className={s.CheckboxIndicator}>
-          <CheckIcon/>
+      <CheckboxRadix.Root className={checkBoxClassName} disabled={disabled} id={'c1'} {...rest}>
+        <CheckboxRadix.Indicator className={s.CheckboxIndicator}>
+          <CheckIcon />
         </CheckboxRadix.Indicator>
       </CheckboxRadix.Root>
-      <label className={checkboxLabelClassName} htmlFor="c1">
+      <label className={checkboxLabelClassName} htmlFor={'c1'}>
         {label}
       </label>
     </div>
   )
 }
 
-export default Checkbox;
+export default Checkbox
