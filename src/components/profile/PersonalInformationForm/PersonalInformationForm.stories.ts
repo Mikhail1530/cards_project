edaa481893c63de101bc3730e94f96c07df23a83
Nikita Alpatov <nikitaa95@mail.ru>
2023-10-28 17:44:11 +0300

    feat: create personal infromation form

--- src/components/profile/PersonalInformationForm/PersonalInformationForm.stories.ts
+++ src/components/profile/PersonalInformationForm/PersonalInformationForm.stories.ts
@@ -10,4 +10,14 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const PersonalInformationFormStories: Story = {}
+export const PersonalInformationFormStories: Story = {
+  args: {
+    avatar:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwRTYbYaWZbc9r4KKdclIUiwO6JRrHgSpRQjB7RTA&s',
+    name: 'bob',
+    email: 'dummy@data.com',
+    changeAvatar: () => console.log('changeAvatar'),
+    saveChangedName: () => console.log('saveChanges'),
+    changeName: () => console.log('changeName'),
+  },
+}
--- src/components/profile/PersonalInformationForm/trash.tsx
+++ src/components/profile/PersonalInformationForm/trash.tsx
@@ -13,7 +13,7 @@ import { useState } from 'react'
 type FormValues = z.infer<typeof personalInformationSchema>
 
 const personalInformationSchema = z.object({
-  name: z.string().min(2, 'Too short nickname').max(25),
+  name: z.string().min(1, 'Too short nickname').max(25),
 })
 
 type PersonalInformationFormProps = {
@@ -27,10 +27,10 @@ type PersonalInformationFormProps = {
 }
 
 export const PersonalInformationForm = ({
-  avatar,
+  avatar = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwRTYbYaWZbc9r4KKdclIUiwO6JRrHgSpRQjB7RTA&s',
   onSubmit,
-  name,
-  email,
+  name = 'bob',
+  email = 'dummy@data.com',
   changeAvatar,
   saveChangedName,
   changeName,
@@ -71,62 +71,59 @@ export const PersonalInformationForm = ({
             <div className={s.avatarContainer}>
               <div className={s.avatar}>
                 <img src={avatar} alt={'avatar'}></img>
-                {isNameEditing ? (
-                  <TextField
-                    {...register('name')}
-                    errorMessage={errors.name?.message}
-                    label={'Nickname'}
-                  />
-                ) : (
+                {!isNameEditing ? (
                   <Button
                     onClick={handleAvatarChange}
                     className={s.avatarEditButton}
                     icon={<EditPencil />}
                     variant={'secondary'}
                   ></Button>
+                ) : (
+                  <TextField
+                    {...register('name')}
+                    errorMessage={errors.name?.message}
+                    label={'Nickname'}
+                  />
                 )}
               </div>
             </div>
-            <div className={s.nameWithEditButtonContainer}>
+            <div className={s.nameWithEditButton}>
               {isNameEditing ? (
                 ''
               ) : (
                 <>
-                  <div className={s.nameWithEditButton}>
-                    <Typography as={'div'} className={s.name} variant={'h1'}>
-                      {name}
-                    </Typography>
-                    <Button
-                      onClick={handleEditName}
-                      className={s.editNameButton}
-                      icon={<EditPencil />}
-                      variant={'secondary'}
-                    ></Button>
-                  </div>
-                  <div>
-                    <Typography as={'div'} className={s.email} variant={'body2'}>
-                      {email}
-                    </Typography>
-                  </div>
+                  <Typography as={'div'} className={s.name} variant={'h1'}>
+                    {name}
+                  </Typography>
+                  <Button
+                    onClick={handleEditName}
+                    className={s.editNameButton}
+                    icon={<EditPencil />}
+                    variant={'secondary'}
+                  ></Button>
+                  <Typography as={'div'} className={s.email} variant={'body2'}>
+                    {email}
+                  </Typography>
                 </>
               )}
             </div>
           </div>
-
-          {isNameEditing ? (
-            <Button
-              onClick={handleSaveChangedName}
-              className={s.but}
-              type="submit"
-              variant={'primary'}
-            >
-              Save changes
-            </Button>
-          ) : (
-            <Button icon={<Logout />} className={s.button_save_logout} variant={'secondary'}>
-              Logout
-            </Button>
-          )}
+          <div>
+            {isNameEditing ? (
+              <Button
+                onClick={handleSaveChangedName}
+                className={s.but}
+                type="submit"
+                variant={'primary'}
+              >
+                Save changes
+              </Button>
+            ) : (
+              <Button icon={<Logout />} className={s.button_save_logout} variant={'secondary'}>
+                Logout
+              </Button>
+            )}
+          </div>
         </div>
       </Card>
     </form>
